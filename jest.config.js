163199693845aba40bf9b9e63470a8865c5198ba
buchiya4th <buchiya4th@gmail.com>
2022-03-06 15:40:44 +0900

    add: storyshots

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,9 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    "\\.(mdx)$": "<rootDir>/.jest/__mocks__/mdx.ts"
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
